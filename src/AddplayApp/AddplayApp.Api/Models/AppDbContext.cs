dae5825a7c0c1827c5c954bf96eca0551b7a5eb3
Irfan <irfanmahmud201@gmail.com>
2025-11-08 22:17:37 +0600

    install packages , setup configurations, generate migration

---
+++ src/AddplayApp/AddplayApp.Api/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace AddplayApp.Api.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt) { }
+    }
+}
---
+++ src/AddplayApp/AddplayApp.Api/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace AddplayApp.Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public int Age { get; set; }
+        public string Email { get; set; } = default!;
+        public DateTime TimeStamp { get; set; }
+    }
+
+}
